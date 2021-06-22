a316dbbca67d33ea31613a9e7bedc57239e2d8a8
Lucas <uprat@live.fr>
2021-06-22 11:03:14 +0200

    Pre-ST4 - Fix variables name

--- space.php
+++ space.php
@@ -12,9 +12,9 @@ if(isset($_GET['id']) || isset($_GET['username'])){
       $smarty->assign('user_req', $user_req);
    }
    if(isset($_GET['username'])){
-      $id_get = $_GET['username'];
+      $username_get = $_GET['username'];
       $user_req = $db->prepare('SELECT * FROM users WHERE username = ?');
-      $user_req->execute(array($id_get));
+      $user_req->execute(array($username_get));
       $user_exist = $user_req->rowCount();
       $smarty->assign('user_req', $user_req);
    }
